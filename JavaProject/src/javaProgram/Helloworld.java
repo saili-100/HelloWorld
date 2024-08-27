69200815517149145e0ee6d14be36b57b232c6c6
saili-100 <sailipatil361@gmail.com>
2024-08-27 16:21:22 +0530

    First commit

---
+++ JavaProject/src/javaProgram/Helloworld.java
@@ -0,0 +1,9 @@
+package javaProgram;
+
+public class Helloworld {
+		public static void main(String[]args) {
+			System.out.println("Hello");
+			System.out.println("Welcome in Java Programming");
+			
+		}
+}
